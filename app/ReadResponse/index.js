469e165cf3e77f173421c8de9beb2165d374c33d
Amadeusz Wieczorek <git@amadeusw.com>
2017-01-05 20:02:20 -0800

    Attempt to bind to table

--- app/ReadResponse/index.js
+++ app/ReadResponse/index.js
@@ -1,6 +1,8 @@
 module.exports = function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
+    var table = context.bindings.weddingTable;
+
     if (req.query.name || (req.body && req.body.name)) {
         res = {
             // status: 200, /* Defaults to 200 */
