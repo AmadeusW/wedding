3862edd967127c2f14c5138c02923baba9e3d5f5
Amadeusz Wieczorek <git@amadeusw.com>
2017-01-07 22:47:47 -0800

    async get query and response!!

--- server/index.js
+++ server/index.js
@@ -8,6 +8,24 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 })
 
+app.get('/status', function (req, res) {
+  if (req.query === undefined || req.query.name === undefined || req.query.magic === undefined)
+    res.status(err.status || 400);
+  console.log("Welcome " + req.query.name + "("+req.query.magic+")")
+/*  
+  var azure = require('azure-storage');
+  var ts = azure.createTableService();
+  console.log(JSON.stringify(ts));
+  var guest = ts.retrieveEntity('wedweb', 'guest', req.query.magic)
+  console.log(JSON.stringify(guest));
+*/
+  res.send("Welcome " + req.query.name + "("+req.query.magic+")");
+})
+
+app.get('/respond', function (req, res) {
+  res.send("respond");
+})
+
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
@@ -33,20 +51,5 @@ app.use(function(err, req, res, next) {
     res.sendFile('D:/wedweb/website/index.html')
 });
 
-
-app.get('/status', function (req, res) {
-    var azure = require('azure-storage');
-    
-    var ts = azure.createTableService();
-    console.log(JSON.stringify(ts));
-    var guest = ts.retrieveEntity('wedweb', 'guest', req.query.magic)
-    console.log(JSON.stringify(guest));
-
-  res.send("basic");
-})
-
-app.get('/respond', function (req, res) {
-  res.send("respond");
-})
 
 app.listen(3000)
