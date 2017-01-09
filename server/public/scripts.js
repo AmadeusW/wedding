9aaa3b7327c24f7a5782ce3eb80162fa42e13e6d
Amadeusz Wieczorek <git@amadeusw.com>
2017-01-08 23:08:45 -0800

    script on page load

--- server/public/scripts.js
+++ server/public/scripts.js
@@ -1,15 +1,39 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "/status?name=PixiePie&magic=1234", true);
-xhr.onload = function (e) {
-  if (xhr.readyState === 4) {
-    if (xhr.status === 200) {
-      console.log(xhr.responseText);
-    } else {
-      console.error(xhr.statusText);
+// On load, check query parameters
+var name = getUrlParameter("name");
+var magic = getUrlParameter("magic");
+if (name !== "" && magic !== "") {
+  go();
+}
+
+function go() {
+  document.getElementById("rsvp-name").innerHTML = name;
+  document.getElementById("rsvp").className = "visible";
+
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", "/status?name="+name+"&magic="+magic, true);
+  xhr.onload = function (e) {
+    if (xhr.readyState === 4) {
+      if (xhr.status === 200) {
+        console.log(xhr.responseText);
+        var entity = JSON.parse(xhr.responseText);
+        document.getElementById("rsvp-music").value = entity.music;
+        document.getElementById("rsvp-food").value = entity.food;
+      } else {
+        console.error(xhr.statusText);
+        document.getElementById("rsvp").className = "";
+      }
     }
-  }
+  };
+  xhr.onerror = function (e) {
+    console.error(xhr.statusText);
+  };
+  xhr.send(null);
+}
+
+
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
 };
-xhr.onerror = function (e) {
-  console.error(xhr.statusText);
-};
-xhr.send(null);
