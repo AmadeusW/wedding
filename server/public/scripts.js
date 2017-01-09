8c886867988512dcb97bf2fac5083853c988b812
Amadeusz Wieczorek <git@amadeusw.com>
2017-01-09 09:22:13 -0800

    uses jquery to make status and respond requests

--- server/public/scripts.js
+++ server/public/scripts.js
@@ -5,32 +5,59 @@ if (name !== "" && magic !== "") {
   go();
 }
 
+function respond() {
+  console.log("Responding...")
+  $.ajax({
+    url: "/respond",
+    data: {
+      name: name,
+      magic: magic
+    },
+    success: function( result ) {
+      console.log(result.response);
+      $( "#rsvp-status" ).html( "'ed ");
+    },
+    error: function(jqHXR, errorStatus, errorThrown) {
+      console.error(jqHXR);
+      console.error(errorThrown);
+      $( "#rsvp-status" ).html( " error " + errorStatus );
+    }
+  });
+}
+
+// Event handlers for inputs
+$( "#rsvp-yes" ).on( "click", respond);
+$( "#rsvp-plusone" ).on( "click", respond);
+$( "#rsvp-no" ).on( "click", respond);
+
 function go() {
   document.getElementById("rsvp-name").innerHTML = name;
   document.getElementById("rsvp").className = "visible";
-
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", "/status?name="+name+"&magic="+magic, true);
-  xhr.onload = function (e) {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        console.log(xhr.responseText);
-        var entity = JSON.parse(xhr.responseText);
-        document.getElementById("rsvp-name").innerHTML = entity.name;
-        document.getElementById("rsvp-music").value = entity.music;
-        document.getElementById("rsvp-food").value = entity.food;
-      } else {
-        console.error(xhr.statusText);
-        document.getElementById("rsvp").className = "";
-      }
+  $( "#rsvp-status" ).html( "..." );
+  $.ajax({
+    url: "/status",
+    data: {
+      name: name,
+      magic: magic
+    },
+    success: function( result ) {
+        document.getElementById("rsvp-name").innerHTML = result.name;
+        document.getElementById("rsvp-music").value = result.music;
+        document.getElementById("rsvp-food").value = result.food;
+        if (result.response !== "-1") {
+          $( "#rsvp-status" ).html( "'ed" );
+        }
+        else {
+          $( "#rsvp-status" ).html( "" );
+        }
+    },
+    error: function(jqHXR, errorStatus, errorThrown) {
+      document.getElementById("rsvp").className = ""; // hide this section
+      console.error(jqHXR);
+      console.error(errorThrown);
     }
-  };
-  xhr.onerror = function (e) {
-    console.error(xhr.statusText);
-  };
-  xhr.send(null);
+  });
 }
-
 
 function getUrlParameter(name) {
     name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
