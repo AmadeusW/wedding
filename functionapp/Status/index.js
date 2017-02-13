2a1c705ac2cf5c1b536f56703801b5f9b0dacfe5
Amadeusz Wieczorek <git@amadeusw.com>
2017-02-12 22:42:17 -0800

    okay then

--- functionapp/Status/index.js
+++ functionapp/Status/index.js
@@ -7,13 +7,13 @@ module.exports = function (context, req) {
 
     if (data) {
         var response = {
-            name: data.Name ? data.Name : "invalid",
-            name2: data.Name2 ? data.Name2 : "",
-            response: data.Response ? data.Response : "",
-            music: data.Music ? data.Music : "",
-            comment: data.Comment ? data.Comment : "",
-            menu1: data.Menu1 ? data.Menu1 : "",
-            menu2: data.Menu2 ? data.Menu2 : "",
+            name: data.name ? data.name : "invalid",
+            name2: data.name2 ? data.name2 : "",
+            response: data.response ? data.response : "",
+            music: data.music ? data.music : "",
+            comment: data.comment ? data.comment : "",
+            menu1: data.menu1 ? data.menu1 : "",
+            menu2: data.menu2 ? data.menu2 : "",
             hotelcode: "3A6CU9J6"
         }
         res = {
